caf56ed9703d3d52ceed51b17cbf7c17a60e1b5a
Piotr Zaczkowski <pzaczkowski@protonmail.com>
2020-12-26 10:42:35 +0100

    Try to read one pin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "debug");
+    }
+    env_logger::Builder::from_default_env()
+        .format_timestamp_millis()
+        .init();
+
+    let gpio = bcm2835_rs::Bcm2835Gpio::new();
+    if gpio.is_none() {
+        log::error!("Failed to initialize chip");
+    }
+    let gpio = gpio.unwrap();
+    log::debug!("Pin state: {:?}", gpio.read(18));
 }
